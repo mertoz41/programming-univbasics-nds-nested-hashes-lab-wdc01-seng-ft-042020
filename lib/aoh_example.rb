419feb3c2e03604a510df760b3f438f295aa5e43
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-03-22 20:54:15 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -36,7 +36,7 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   
-  return aoh[0][:key] = 3
+  return aoh[row][key] = new_value.int
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
